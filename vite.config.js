d047a0bb4a466981ce50158b5f0cf86aa9ec3f1b
Darren <darren@darrenwhitlen.com>
2022-08-07 15:20:19 +0100

    initial

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4000,
+    base: '/test/',
+  }
+})
